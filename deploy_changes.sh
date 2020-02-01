a87ac0b8c61a24484f6f9b95ccca24077ea211c3
Aaron Edwards <aaron@cloudgenix.com>
2020-01-31 16:36:31 -0800

    test

--- deploy_changes.sh
+++ deploy_changes.sh
@@ -10,10 +10,13 @@
 # AUTH_TOKEN = Valid tenant_super CloudGenix Auth Token
 # TRAVIS_COMMIT = Current commit hash
 
+EXIT_CODE=0
+
 # Debug script (if needed)
-set -x
+#set -x
 
 # Set up git for log commit back to master.
+echo "Setting GIT authentication/origin.."
 git config --global user.email "travisci-worker-ebob9@travis-ci.org"
 git config --global user.name "travisci-worker-ebob9"
 
@@ -22,31 +25,40 @@ git remote rm origin
 # Add new "origin" with access token in the git URL for authentication
 git remote add origin "https://travisci-worker-ebob9:${GITHUB_REPO_TOKEN}@github.com/ebob9/travis-sandbox.git" > /dev/null 2>&1
 
-# DEBUG - find out why things arent working
-git remote get-url --all origin
-git show-ref -s in_prod
+## DEBUG - find out why things arent working
+#git remote get-url --all origin
+#git show-ref -s in_prod
 
 # Get latest commit tagged in production
 CGX_COMMIT_IN_PROD=$(git show-ref -s in_prod)
-
+echo "Latest 'in_prod' commit: ${CGX_COMMIT_IN_PROD}"
 
 # Get modified from current master commit and latest in_prod commit.
 MODIFIED_CONFIGS=$(git diff "${CGX_COMMIT_IN_PROD}" "${TRAVIS_COMMIT}" --diff-filter=ACMR --name-status | cut -f2 \
                    | grep 'configurations\/.*\.yml')
 
 # execute the changes
-echo "PROD_TO_CURRENT: ${CGX_COMMIT_IN_PROD}...${TRAVIS_COMMIT}"
-echo "MODIFIED_CONFIGS: ${MODIFIED_CONFIGS}"
+echo "Commit diff check range: ${CGX_COMMIT_IN_PROD}...${TRAVIS_COMMIT}"
+echo "Configuration Files Modified: ${MODIFIED_CONFIGS}"
 
 # create tmp logs
 mkdir /tmp/logs
 
 for SITE_CONFIG in ${MODIFIED_CONFIGS}
   do
-    echo "${SITE_CONFIG}-$RANDOM" > "/tmp/logs/$(basename "${SITE_CONFIG}")" 2>&1
+    SITE_CONFIG_FILE=$(basename "${SITE_CONFIG}")
+    echo -n "Executing ${SITE_CONFIG_FILE} Configuration: "
+    if echo "${SITE_CONFIG}-$RANDOM" > "/tmp/logs/${SITE_CONFIG_FILE}.log" 2>&1
+      then
+        echo "Success. "
+      else
+        echo "Failed, code $?. "
+        EXIT_CODE=1
+    fi
   done
 
 # delete current in_prod tag
+echo "Updating 'in_prod' tag in Github.."
 git tag -d in_prod
 git push origin :refs/tags/in_prod
 
@@ -54,7 +66,9 @@ git push origin :refs/tags/in_prod
 git tag in_prod
 git push origin refs/tags/in_prod
 
+
 # switch to logs
+echo "Saving logs to origin/logs.."
 git checkout -b logs
 git fetch --all
 git branch -u origin/logs
@@ -68,11 +82,15 @@ cp -a /tmp/logs/* logs/
 
 # push logs to logs repository
 git add logs/*
-git commit -m 'Build Log Results [ci skip]'
+git commit -m 'Configuration Log Results [ci skip]'
 git push origin logs
 
 ## Debug push items
 #git log --full-history
 #git reflog
 #git remote -v
+
+echo "Finished!"
+exit ${EXIT_CODE}
+
 
